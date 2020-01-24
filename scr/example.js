968c4dd9fbb7e30b8b6dedbf5999e8e48fc50f33
perssBest <49908303+perssBest@users.noreply.github.com>
2020-01-24 22:17:28 +0500

    Create example.js

---
+++ scr/example.js
@@ -0,0 +1,15 @@
+const
+    { RichEmbed } = require('discord.js'),
+    config = require('../config.json')
+module.exports = {
+    run: async (client,message,args) => {
+
+  },
+    cmd: {
+      name: " ",
+      aliases: [" "],
+      category: " ",
+      desc: "",
+      usage: " "
+    }
+}
